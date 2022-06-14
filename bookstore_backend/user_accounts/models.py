fe80479759fe5af11481fb1fa8201bdf970f8080
LaffSteven <steve.laff@gmail.com>
2022-06-14 15:54:59 -0400

    temporarily removing cart from model

--- bookstore_backend/user_accounts/models.py
+++ bookstore_backend/user_accounts/models.py
@@ -10,4 +10,4 @@ class UserAccount(models.Model):
     first_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32)
     staff = models.BooleanField()
-    cart = models.JSONField(encoder=None, null=True)
+    # cart = models.JSONField(encoder=None, null=True)

21e8dd17653ef882bea60643b53e808d0930d3e1
CarolineDuffett <ckduffett@gmail.com>
2022-06-16 21:56:25 -0400

    commented out what was changed during homework review. Do not get rid of because it was helpful

--- bookstore_backend/books_api/views.py
+++ bookstore_backend/books_api/views.py
@@ -30,25 +30,25 @@ class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ReviewSerializer
 
 
-#Choices are: book, book_id, id, review, user_id
-def getBookReviews(request):
-    #print(request)
-    if request.method=='GET':
-        #print(str(request) + " is the reuqest")
-        body_unicode = request.body.decode('utf-8')
-        jsonRequest = json.loads(body_unicode)
-        bookID = int(jsonRequest['bookID'])
-        #print(type(jsonRequest))
-        #print(type(bookID))
-        # if Book.objects.get(id=book.id):
-        #print(str(bookID) + " is the book id")
-        #print(Review.objects.order_by('book_id'))
-        reviewList = list(Review.objects.filter(book_id=bookID).values('id', 'review', 'user_id', 'book_id'))
-        #book_id==bookID
-        print(reviewList)
-        #return JsonResponse(reviewList, safe=False)
-        #return JsonResponse(reviewList, safe=False)
-
-        # else:
-        #     return JsonResponse({})
-    return JsonResponse(reviewList, safe=False)
+# #Choices are: book, book_id, id, review, user_id
+# def getBookReviews(request):
+#     #print(request)
+#     if request.method=='GET':
+#         #print(str(request) + " is the reuqest")
+#         body_unicode = request.body.decode('utf-8')
+#         jsonRequest = json.loads(body_unicode)
+#         bookID = int(jsonRequest['bookID'])
+#         #print(type(jsonRequest))
+#         #print(type(bookID))
+#         # if Book.objects.get(id=book.id):
+#         #print(str(bookID) + " is the book id")
+#         #print(Review.objects.order_by('book_id'))
+#         reviewList = list(Review.objects.filter(book_id=bookID).values('id', 'review', 'user_id', 'book_id'))
+#         #book_id==bookID
+#         print(reviewList)
+#         #return JsonResponse(reviewList, safe=False)
+#         #return JsonResponse(reviewList, safe=False)
+#
+#         # else:
+#         #     return JsonResponse({})
+#     return JsonResponse(reviewList, safe=False)

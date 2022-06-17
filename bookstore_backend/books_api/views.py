9adbd37cc30984f44e6a72c3e268f5ab14cdf704
CarolineDuffett <ckduffett@gmail.com>
2022-06-16 21:27:12 -0400

    code after homework help attempt. Can get it in postman but not react

--- bookstore_backend/books_api/views.py
+++ bookstore_backend/books_api/views.py
@@ -32,21 +32,23 @@ class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
 
 #Choices are: book, book_id, id, review, user_id
 def getBookReviews(request):
-    print(request)
-    # if request.method=='GET':
-    #     #print(str(request) + " is the reuqest")
-    #     jsonRequest = json.loads(request.body)
-    #     bookID = int(jsonRequest['bookID'])
-    #     # if Book.objects.get(id=book.id):
-    #     #print(str(bookID) + " is the book id")
-    #     #print(Review.objects.order_by('book_id'))
-    #     reviewList = list(Review.objects.filter(book_id=bookID).values('id', 'review', 'user_id', 'book_id'))
-    #     #book_id==bookID
-    #     print(reviewList)
-    #     #return JsonResponse(reviewList, safe=False)
-    #     return JsonResponse(reviewList, safe=False)
-    #
-    #     # else:
-    #     #     return JsonResponse({})
-    # else:
-    #     return JsonResponse({})
+    #print(request)
+    if request.method=='GET':
+        #print(str(request) + " is the reuqest")
+        body_unicode = request.body.decode('utf-8')
+        jsonRequest = json.loads(body_unicode)
+        bookID = int(jsonRequest['bookID'])
+        #print(type(jsonRequest))
+        #print(type(bookID))
+        # if Book.objects.get(id=book.id):
+        #print(str(bookID) + " is the book id")
+        #print(Review.objects.order_by('book_id'))
+        reviewList = list(Review.objects.filter(book_id=bookID).values('id', 'review', 'user_id', 'book_id'))
+        #book_id==bookID
+        print(reviewList)
+        #return JsonResponse(reviewList, safe=False)
+        #return JsonResponse(reviewList, safe=False)
+
+        # else:
+        #     return JsonResponse({})
+    return JsonResponse(reviewList, safe=False)
